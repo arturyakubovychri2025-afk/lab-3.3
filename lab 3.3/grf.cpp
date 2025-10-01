0fa5be41dd7b062b2f3bb1fba34470ad47cc4aa8
arturyakubovychri2025-afk <artur.yakubovych.ri.2025@lpnu.ua>
2025-10-01 12:06:59 +0300

    4.3

--- lab 3.3/grf.cpp
+++ lab 3.3/grf.cpp
@@ -1,4 +1,4 @@
-//Lab_03_3.cp
+//Lab_04_4.cp
 // Якубович Артур
 //Лабораторна робота №3.3
 //Розгалуження, задане графіком функції.
@@ -6,32 +6,43 @@
 
 #include <iostream>
 #include <cmath>
-
+#include <iomanip>
+// xk=8   xp=-10  R=2  x=-10  dx=2
 using namespace std;
 
 int main()
 {
-	double x;
+	double x, xp, xk, dx;
 	double R;
 	double y;
-
+	cout << "xk = "; cin >> xk;
+	cout << "xp = "; cin >> xp;
 	cout << "R = "; cin >> R;
 	cout << "x = "; cin >> x;
-	
-	//розгалуження в повній формі
-	if (x >= ( - 8 - R) && x <= ( - 8 + R))
-		y = -sqrt((R*R) - pow(x - (-8), 2)) - R;
-	else
-		if (x > ( - 8 + R) && x <= 2)
-			y = x - R;
+	cout << "dx = "; cin >> dx;
+	y = 0;
+	cout << fixed;
+	cout << "----------------------------------" << endl;
+	cout << "|" << setw(10) << "R" << "|" << setw(10) << "x" << "|"  << setw(10) << "y" << "|" << endl;
+	cout << "----------------------------------" << endl;
+	x = xp;
+	while (x <= xk) {
+		if (x >= (-8 - R) && x <= (-8 + R))
+			y = -sqrt((R * R) - pow(x - (-8), 2)) - R;
 		else
-			if (x > 2  && x <= 6)
-				y = 0;
+			if (x > (-8 + R) && x <= 2)
+				y = x - R;
 			else
-				if (x > 6 && x <= 8)
-					y = pow((x - 6), 2);
-	cout << endl;
-	cout << "y = " << y << endl;
+				if (x > 2 && x <= 6)
+					y = 0;
+				else
+					if (x > 6 && x <= 8)
+						y = pow((x - 6), 2);
+		cout << "|" << setw(10) << setprecision(2) << R << "|" << setw(10) << setprecision(2) << x << "|" << setw(10) << setprecision(2) << y << "|" << endl;
+		x += dx;
+	}
+
+	cout << "----------------------------------" << endl;
 	cin.get();
 	return 0;
 }
